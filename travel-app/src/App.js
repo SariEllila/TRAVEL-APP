3b51078a77ee1dfecaf37059b9caf27248374594
sari.ellila <ellila89@hotmail.com>
2024-08-22 12:37:40 +0300

    Finished Quiz functionality fix - 0822

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import DestCards from './components/DestCards';
@@ -13,232 +13,208 @@ import NewsPages from './components/NewsPages';
 import Footer from './components/Footer';
 
 function App() {
-  const [selectedDestinationId, setSelectedDestinationId] = useState(null);
-  const [selectedNewsId, setSelectedNewsId] = useState(null);
-  const [showQuiz, setShowQuiz] = useState(false);
-  const [viewType, setViewType] = useState(''); // Manage the view type
-  const [showQuizContainer, setShowQuizContainer] = useState(true);
-  const [savedAnswers, setSavedAnswers] = useState([]);
-  const [quizResult, setQuizResult] = useState(null);
+    const [selectedDestinationId, setSelectedDestinationId] = useState(null);
+    const [selectedNewsId, setSelectedNewsId] = useState(null);
+    const [showQuiz, setShowQuiz] = useState(false);
+    const [viewType, setViewType] = useState(''); // New state to manage the view type (destinations, news, quiz)
+    const [showQuizContainer, setShowQuizContainer] = useState(true);
+    const [savedAnswers, setSavedAnswers] = useState([]);
 
-  function handleDestClick(id) {
-    setSelectedDestinationId(id);
-    setSelectedNewsId(null); // Reset selected news
-    setShowQuiz(false); // Hide quiz
-    setViewType('destinations'); // Set view to destinations
-  }
-
-  function handleNewsClick(id) {
-    setSelectedNewsId(id);
-    setSelectedDestinationId(null); // Reset selected destination
-    setShowQuiz(false); // Hide quiz
-    setViewType('news'); // Set view to news
-  }
-
-  function handleQuizClick() {
-    setShowQuiz(true);
-    setSelectedDestinationId(null); // Reset selected destination
-    setSelectedNewsId(null); // Reset selected news
-    setViewType('quiz'); // Set view to quiz
-  }
-
-  function handleBackToNews() {
-    setSelectedNewsId(null); // Reset selected news
-    setSelectedDestinationId(null); // Reset selected destination
-    setShowQuiz(false); // Hide quiz
-    setViewType(''); // Set view back to default
-  }
-
-  function saveAnswers(answer) {
-    setSavedAnswers(prevAnswers => [...prevAnswers, answer]);
-  }
-
-  function handleQuizCompletion() {
-    const result = calculateResult(savedAnswers);
-    setQuizResult(result);
-  }
-
-  function calculateResult(answers) {
-    const frequency = {};
-    answers.forEach(answer => {
-      if (frequency[answer]) {
-        frequency[answer] += 1;
-      } else {
-        frequency[answer] = 1;
-      }
-    });
-    // Get the answer with the highest frequency
-    const maxFrequency = Math.max(...Object.values(frequency));
-    const result = Object.keys(frequency).find(key => frequency[key] === maxFrequency);
-    return result.replace('answer', ''); // Remove 'answer' prefix
-  }
-
-  function closeQuizResult() {
-    setQuizResult(null);
-    setShowQuiz(false);
-    setShowQuizContainer(true);
-  }
-
-  const selectedDest = DestinationsData.find(item => item.id === selectedDestinationId);
-  const selectedNews = NewsData.find(item => item.id === selectedNewsId);
-
-  const destCards = DestinationsData.map(item => (
-    <DestCards
-      key={item.id}
-      id={item.id}
-      img={item.img}
-      city={item.city}
-      handleDestClick={() => handleDestClick(item.id)}
-    />
-  ));
-
-  const destPages = selectedDest ? (
-    <DestPages
-      key={selectedDest.id}
-      id={selectedDest.id}
-      img={selectedDest.img}
-      city={selectedDest.city}
-      location={selectedDest.location}
-      distanceFromTokyo={selectedDest.distanceFromTokyo}
-      population={selectedDest.population}
-      description={selectedDest.description}
-      popularSights={selectedDest.popularSights}
-    />
-  ) : null;
-
-  const newsCards = NewsData.map(item => (
-    <NewsCards
-      key={item.id}
-      img={item.img}
-      title={item.title}
-      text={item.text}
-      date={item.date}
-      handleNewsClick={() => handleNewsClick(item.id)}
-    />
-  ));
-
-  const newsPages = selectedNews ? (
-    <NewsPages
-      key={selectedNews.id}
-      img={selectedNews.img}
-      title={selectedNews.title}
-      date={selectedNews.date}
-      text={selectedNews.text}
-      onBackToNews={handleBackToNews}
-    />
-  ) : null;
-
-  const quizPage = showQuiz ? (
-    <div className="quiz-container">
-      <Quiz
-        questions={QuizData}
-        saveAnswers={saveAnswers}
-        handleQuizCompletion={handleQuizCompletion}
-      />
-      {quizResult && (
-        <div className="quiz-result-modal">
-          <div className="quiz-result-content">
-            <h2>Your result: {quizResult}</h2>
-            <button onClick={closeQuizResult}>Close</button>
-          </div>
-        </div>
-      )}
-    </div>
-  ) : null;
-
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const containerRef = useRef();
-  const item_width = 400;
-
-  function debounce(func, delay) {
-    let timeoutId;
-    return function (...args) {
-      if (timeoutId) clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  }
-
-  const handleScroll = debounce((scrollAmount) => {
-    if (containerRef.current) {
-      const maxScrollLeft = containerRef.current.scrollWidth - containerRef.current.clientWidth;
-      const newScrollPosition = Math.min(Math.max(scrollPosition + scrollAmount, 0), maxScrollLeft);
-      setScrollPosition(newScrollPosition);
-      containerRef.current.scrollLeft = newScrollPosition;
+    function handleDestClick(id) {
+        setSelectedDestinationId(id);
+        setSelectedNewsId(null); // Reset selected news
+        setShowQuiz(false); // Hide quiz
+        setViewType('destinations'); // Set view to destinations
     }
-  }, 100);
 
-  useEffect(() => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft = scrollPosition;
+    function handleNewsClick(id) {
+        setSelectedNewsId(id);
+        setSelectedDestinationId(null); // Reset selected destination
+        setShowQuiz(false); // Hide quiz
+        setViewType('news'); // Set view to news
     }
-  }, [scrollPosition]);
 
-  return (
-    <div>
-      <Header />
+    function handleQuizClick() {
+        setShowQuiz(true);
+        setSelectedDestinationId(null); // Reset selected destination
+        setSelectedNewsId(null); // Reset selected news
+        setViewType('quiz'); // Set view to quiz
+    }
 
-      <div className="browse-dest">
-        <h1 className="browse-dest-text">
-          Browse <span className="light-coral" style={{ fontSize: '1.1em' }}>Destinations</span>
-        </h1>
+    function handleBackToNews() {
+        setSelectedNewsId(null); // Reset selected news
+        setSelectedDestinationId(null); // Reset selected destination
+        setShowQuiz(false); // Hide quiz
+        setViewType(''); // Set view back to default
+    }
 
-        <div className="destinations-scroll">
-          <div ref={containerRef} style={{
-            width: "90vw",
-            overflowX: "scroll",
-            scrollBehavior: "smooth"
-          }}>
-            <div className="destcards-box">
-              {destCards}
+    function saveAnswers(answer) {
+        setSavedAnswers(prevAnswers => [...prevAnswers, answer]);
+    }
+
+    function navigateToDestination(cityName) {
+        // Find the destination ID based on the city name
+        const destination = DestinationsData.find(item => item.city === cityName);
+        if (destination) {
+            setSelectedDestinationId(destination.id);
+            setSelectedNewsId(null); // Reset selected news
+            setShowQuiz(false); // Hide quiz
+            setViewType('destinations'); // Navigate to destination view
+        } else {
+            console.error('Destination not found for city:', cityName);
+        }
+    }
+
+    const selectedDest = DestinationsData.find(item => item.id === selectedDestinationId);
+    const selectedNews = NewsData.find(item => item.id === selectedNewsId);
+
+    const destCards = DestinationsData.map(item => (
+        <DestCards
+            key={item.id}
+            id={item.id}
+            img={item.img}
+            city={item.city}
+            handleDestClick={() => handleDestClick(item.id)}
+        />
+    ));
+
+    const destPages = selectedDest ? (
+        <DestPages
+            key={selectedDest.id}
+            id={selectedDest.id}
+            img={selectedDest.img}
+            city={selectedDest.city}
+            location={selectedDest.location}
+            distanceFromTokyo={selectedDest.distanceFromTokyo}
+            population={selectedDest.population}
+            description={selectedDest.description}
+            popularSights={selectedDest.popularSights}
+        />
+    ) : null;
+
+    const newsCards = NewsData.map(item => (
+        <NewsCards
+            key={item.id}
+            img={item.img}
+            title={item.title}
+            text={item.text}
+            date={item.date}
+            handleNewsClick={() => handleNewsClick(item.id)}
+        />
+    ));
+
+    const newsPages = selectedNews ? (
+        <NewsPages
+            key={selectedNews.id}
+            img={selectedNews.img}
+            title={selectedNews.title}
+            date={selectedNews.date}
+            text={selectedNews.text}
+            onBackToNews={handleBackToNews} // Pass the function as a prop
+        />
+    ) : null;
+
+    const quizPage = (
+        <Quiz
+            questions={QuizData}
+            saveAnswers={saveAnswers}
+            handleQuizCompletion={() => setShowQuiz(false)}
+            navigateToDestination={navigateToDestination}
+        />
+    );
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const containerRef = useRef();
+    const item_width = 400;
+
+    function debounce(func, delay) {
+        let timeoutId;
+        return function (...args) {
+            if (timeoutId) clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    }
+
+    const handleScroll = debounce((scrollAmount) => {
+        if (containerRef.current) {
+            const maxScrollLeft = containerRef.current.scrollWidth - containerRef.current.clientWidth;
+            const newScrollPosition = Math.min(Math.max(scrollPosition + scrollAmount, 0), maxScrollLeft);
+            setScrollPosition(newScrollPosition);
+            containerRef.current.scrollLeft = newScrollPosition;
+        }
+    }, 100);
+
+    useEffect(() => {
+        if (containerRef.current) {
+            containerRef.current.scrollLeft = scrollPosition;
+        }
+    }, [scrollPosition]);
+
+    return (
+        <div>
+            <Header />
+
+            <div className="browse-dest">
+                <h1 className="browse-dest-text">
+                    Browse <span className="light-coral" style={{ fontSize: '1.1em' }}>Destinations</span>
+                </h1>
+
+                <div className="destinations-scroll">
+                    <div ref={containerRef} style={{
+                        width: "90vw",
+                        overflowX: "scroll",
+                        scrollBehavior: "smooth"
+                    }}>
+                        <div className="destcards-box">
+                            {destCards}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="dest-scroll-buttons">
-        <button onClick={() => handleScroll(-item_width)}>←</button>
-        <button onClick={() => handleScroll(item_width)}>→</button>
-      </div>
-
-      {showQuizContainer && !showQuiz && (
-        <div className="quiz-container">
-          <div className="quiz-link-text">
-            <h1>Do you want to know which destination is best for <em>YOU?</em></h1>
-            <h1 onClick={handleQuizClick}>
-              <span className="take-quiz-span"> Take the QUIZ</span>
-            </h1>
-          </div>
-        </div>
-      )}
-
-      <div className="news-weather-container">
-        <div className="news-quiz-container">
-          {viewType === '' && (
-            <h1>Japan <span className="news-title-span">News</span></h1>
-          )}
-          {viewType === 'destinations' && destPages}
-          {viewType === 'news' && newsPages}
-          {viewType === 'quiz' && quizPage}
-          {!viewType && (
-            <div>
-              {newsCards}
+            <div className="dest-scroll-buttons">
+                <button onClick={() => handleScroll(-item_width)}>←</button>
+                <button onClick={() => handleScroll(item_width)}>→</button>
             </div>
-          )}
-        </div>
 
-        <div className="weather-container">
-          <div className="weather-subcontainer">
-            <h1>How's the <span className="light-coral">Weather?</span></h1>
-            <Weather />
-          </div>
-        </div>
-      </div>
+            {showQuizContainer && !showQuiz && (
+                <div className="quiz-container">
+                    <div className="quiz-link-text">
+                        <h1>Do you want to know which destination is best for <em>YOU?</em></h1>
+                        <h1 onClick={handleQuizClick}>
+                            <span className="take-quiz-span"> Take the QUIZ</span>
+                        </h1>
+                    </div>
+                </div>
+            )}
 
-      <Footer />
-    </div>
-  );
+            <div className="news-weather-container">
+                <div className="news-quiz-container">
+                    {viewType === '' && (
+                        <h1>Japan <span className="news-title-span">News</span></h1>
+                    )}
+                    {viewType === 'destinations' && destPages}
+                    {viewType === 'news' && newsPages}
+                    {viewType === 'quiz' && quizPage}
+                    {!viewType && (
+                        <div>
+                            {newsCards}
+                        </div>
+                    )}
+                </div>
+
+                <div className="weather-container">
+                    <div className="weather-subcontainer">
+                        <h1>How's the <span className="light-coral">Weather?</span></h1>
+                        <Weather />
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- travel-app/src/components/Quiz.js
+++ travel-app/src/components/Quiz.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Quiz({ questions, saveAnswers, handleQuizCompletion }) {
+export default function Quiz({ questions, saveAnswers, handleQuizCompletion, navigateToDestination }) {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const [answers, setAnswers] = useState({});
+    const [answers, setAnswers] = useState([]);
+    const [result, setResult] = useState(null);
 
     useEffect(() => {
         // Reset selected answer when question changes
@@ -17,42 +18,61 @@ export default function Quiz({ questions, saveAnswers, handleQuizCompletion }) {
     function handleNextButtonClick() {
         if (selectedAnswer !== null) {
             // Save the answer for the current question
-            setAnswers(prevAnswers => ({
-                ...prevAnswers,
-                [currentQuestionIndex]: selectedAnswer
-            }));
-
-            // Save the answer to global state
-            saveAnswers(selectedAnswer);
-
-            setSelectedAnswer(null);
+            setAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
 
             // Move to the next question or finish quiz
-            if (currentQuestionIndex === questions.length - 1) {
-                handleQuizCompletion();
+            if (currentQuestionIndex < questions.length - 1) {
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
             } else {
-                setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+                // Calculate the result after all questions are answered
+                calculateResult();
             }
         }
     }
 
-    const question = questions[currentQuestionIndex];
+    function calculateResult() {
+        // Count the number of times each answer is selected
+        const answerCounts = {};
+        answers.forEach(answer => {
+            answerCounts[answer] = (answerCounts[answer] || 0) + 1;
+        });
+
+        // Find the answer with the highest count
+        const mostFrequentAnswer = Object.keys(answerCounts).reduce((a, b) => answerCounts[a] > answerCounts[b] ? a : b);
+
+        // Extract city name from the answer key (e.g., 'answerFukuoka' -> 'Fukuoka')
+        const city = mostFrequentAnswer.replace('answer', '');
+
+        setResult(city); // Set the result
+    }
+
+    function handleGoToDestinationClick() {
+        // Navigate to the destination page based on the result
+        if (result) {
+            navigateToDestination(result);
+        }
+    }
 
     return (
-        <div className="quiz-content">
-            {question ? (
+        <div>
+            {result ? (
+                <div className="modal">
+                    <h1>Your destination is {result}!</h1>
+                    <button onClick={handleGoToDestinationClick}>Go to destination page</button>
+                </div>
+            ) : (
                 <div>
-                    <h1>{question.question}</h1>
+                    <h1>{questions[currentQuestionIndex].question}</h1>
                     <form>
-                        {Object.entries(question.answers).map(([key, answer], index) => (
+                        {Object.entries(questions[currentQuestionIndex].answers).map(([key, answer], index) => (
                             <div key={key}>
-                                <input 
-                                    type="radio" 
-                                    id={`answer${index + 1}`} 
-                                    name="answer" 
-                                    data-key={key} 
-                                    onChange={handleAnswerSelection}
+                                <input
+                                    type="radio"
+                                    id={`answer${index + 1}`}
+                                    name="answer"
+                                    data-key={key}
                                     checked={selectedAnswer === key}
+                                    onChange={handleAnswerSelection}
                                 />
                                 <label htmlFor={`answer${index + 1}`}>{answer}</label>
                             </div>
@@ -62,8 +82,6 @@ export default function Quiz({ questions, saveAnswers, handleQuizCompletion }) {
                         {currentQuestionIndex === questions.length - 1 ? "Finish Quiz" : "Next"}
                     </button>
                 </div>
-            ) : (
-                <div>Loading...</div>
             )}
         </div>
     );
