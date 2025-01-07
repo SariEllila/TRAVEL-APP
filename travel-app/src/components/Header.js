f2a90308d128ec376e1fe55534bb9157c1318ffd
sari.ellila <ellila89@hotmail.com>
2025-01-07 13:27:04 +0100

    fixing router issue for home route - 070125

--- travel-app/src/components/Header.js
+++ travel-app/src/components/Header.js
@@ -4,7 +4,7 @@ import JAPANBANNER2 from '../images/JAPANBANNER2.png';
 export default function Header() {
     return (
 <div className="banner-container">
-    <a href="./App.js">
+    <a href="./">
         <img src={JAPANBANNER2} alt="Tokyo skyline in black and watercolors in the back" className="banner-img"/>
     </a>
 </div>
